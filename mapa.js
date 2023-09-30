4c2d94c615a4d14420dbe22cc1081dc0d4a4ce83
Rodrigo Robles <randresrobles@gmail.com>
2023-09-30 16:54:25 -0300

    Add files via upload

---
+++ mapa.js
@@ -0,0 +1,92 @@
+// Inicializar el mapa con una vista central (setview) y un zoom (3)
+var map = L.map('map').setView([-34.7909491,-58.3370924], 13, );
+//Agregar mapa base de IGN
+var ign_clasico = L.tileLayer('https://wms.ign.gob.ar/geoserver/gwc/service/tms/1.0.0/capabaseargenmap@EPSG%3A3857@png/{z}/{x}/{-y}.png', {
+    attribution: '<a href="http://leafletjs.com" title="A JS library for interactive maps">Leaflet</a> | <a href="http://www.ign.gob.ar/AreaServicios/Argenmap/IntroduccionV2" target="_blank">Instituto Geográfico Nacional</a> + <a href="http://www.osm.org/copyright" target="_blank">OpenStreetMap</a>',
+    minZoom: 10,
+    maxZoom: 18,
+   }).addTo(map);
+
+// ZOOM A MARCADOR
+var xxy = function(e){
+    var coord = e.latlng.toString().split(',');
+    var lat = coord[0].split('(');
+    var lng = coord[1].split(')');
+    console.log("You clicked the map at latitude: " + lat[1] + " and longitude:" + lng[0]);
+    map.flyTo([lat[1], lng[0]], 16, {animate: true,duration: 2 });}
+
+var azul = L.icon
+		({
+		iconUrl: 'https://atodaspartes.ar/wp-content/uploads/2023/09/claypole.png',
+		shadowUrl: '',
+		iconSize: [30, 30], //recomendado
+		iconAnchor: [15, 30], //la itad e igual
+		popupAnchor: [0, -10],
+		//shadowSize: [50, 50] // igual a icono
+		});
+
+var lospibesdesolano = L.marker([-34.7879085,-58.3179796], {icon: azul},13).bindPopup("<strong>Los Pibes de Solano</strong>" + "<br/>" + "Ubicacion: Clavel N°630, Solano" + "<br/>"  + "<img src= 'https://atodaspartes.ar/wp-content/uploads/2023/09/IMG-20230929-WA0049.jpg'/>" + "<A HREF='https://forms.gle/33CVhuAnv3w9jPVZA'target=_blank> Subí el mural de tu barrio </A> " ).on('click', xxy).addTo(map);
+
+
+var baseMaps = [
+			                { 
+								groupName : "Mapas Base",
+								expanded : true,
+								layers    : {
+									
+									
+						
+								}
+					        },
+
+                        ]
+
+var overlays = [
+							 {
+								groupName : "MURALES DEL TAMBO",
+								expanded : true,
+								layers    : { 
+									"Los PIbes de Solano":lospibesdesolano,
+									
+									
+								}	
+                             },
+							 
+                             {
+								groupName : "servicios bancarios",
+								expanded : true,
+								layers    : { 
+									
+								}	
+                             },
+							 {
+								groupName : "Recorridos en Bicicleta",
+								expanded : true,
+								layers    : { 
+									
+									
+									
+								}	
+                             },
+                             
+                             ]
+
+// configuracion de los estilos de capas
+
+
+var options = {
+				container_width 	: "300px",
+				group_maxHeight     : "150px",
+				//container_maxHeight : "350px", 
+				exclusive       	: false,
+				collapsed : true, 
+				position: 'topright'
+			};
+		
+var control = L.Control.styledLayerControl(baseMaps, overlays, options);
+map.addControl(control);
+map.scrollWheelZoom.disable();
+
+			
+
+			
